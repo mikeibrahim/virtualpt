ec5a3878b42cb56c8bd6beb72ae936dbb1044485
Om Mahesh <2om.mahesh@gmail.com>
2023-01-22 10:30:23 -0800

    maybe possibly a progress bar hopefully please

--- src/pages/Workout/Workout.jsx
+++ src/pages/Workout/Workout.jsx
@@ -1,26 +1,28 @@
 import Stream from '../Stream/Stream';
 import workout from './workout.json';
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+
 
 export default function Workout() {
-  const [currReps, changeRep] = useState(0);
-  const [currSets, changeSets] = useState(0);
-  const [currWorkout, changeWorkout] = useState(0);
-  const { id, title, reps, sets, vpt } = workout[0];
+  let currReps = 0, currSets = 0, currWorkout = 0;
+  const workoutTitle = useRef();
+  const upcoming = useRef();
+  const progressbar = useRef();
 
   const nextRep = () => {
     console.log("nextRep");
-    changeRep((currReps + 1));
-    if ((currReps + 1) % reps === 0) {
-      changeSets(currSets + 1);
-    }
-    if (currSets + 1 === sets) {
-      changeWorkout(currWorkout + 1);
-      id = workout[currWorkout].name;
-      title = workout[currWorkout].title;
-      reps = workout[currWorkout].reps;
-      sets = workout[currWorkout].sets;
-      vpt = workout[currWorkout].vpt;
+    currReps++;
+    currReps %= workout[currWorkout].reps;
+    if (currReps === 0) {
+      currSets++;
+      currSets %= workout[currWorkout].sets;
+      console.log(currSets);
+      if (currSets === 0) {
+        currWorkout++;
+        workoutTitle.current.innerText = "Current: " + workout[currWorkout].title;
+        upcoming.current.innerText = currWorkout + 1 < workout.length ? "Upcoming: " + workout[currWorkout + 1].title : "Last set!";
+      }
     }
   };
 
@@ -28,22 +30,20 @@ export default function Workout() {
     console.log("alert");
   };
 
-  const draw = () => {
-    window.requestAnimationFrame(draw);
-  };
-  draw();
-
   return (
     <>
-      <Stream id={id} vpt={vpt} nextRep={nextRep} alert={alert} />
-      {
-        vpt ?
-          <div id="info-container">
-            <p id="workout-title">Current Set: {currReps}</p>
-            <p id="upcoming">Upcoming: {currWorkout + 1 < workout.length ? workout[currWorkout + 1].title : "Last set!"}</p>
-          </div> :
-          <button id="next-button">Next →</button>
-      }
+      <Stream
+        id={workout[currWorkout].id}
+        vpt={workout[currWorkout].vpt}
+        nextRep={nextRep}
+        alert={alert}
+        changePercentage={p => progressbar.style["--value"] === p} />
+      <div id="info-container">
+        <p id="workout-title" ref={workoutTitle}>Current Set: {workout[0].title}</p>
+        <p id="upcoming" ref={upcoming}>Upcoming: {workout[1].title}</p>
+      </div>
+      <div ref={progressbar} role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style={{ "--value": 50 }}></div>
+      {/* <button id="next-button">Next →</button> */}
       <canvas />
     </>
   );
